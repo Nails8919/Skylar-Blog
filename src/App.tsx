fd78e776e2d2530af14943a9db439d64e550db8f
nails8919 <shorty_love_12@hotmail.com>
2025-09-26 10:48:39 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="w-screen h-screen bg-amber-600 flex flex-col items-center">
+      <h1 className="text-3xl font-bold text-black permanent-marker-regular">
+        Our Blog
+      </h1>
+      <div className="flex flex-row items-">
+        <div>2</div>
+        <div>1</div>
+        <div>1</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
